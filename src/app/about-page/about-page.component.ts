5442c692901fa6813070954b4a6c31eb3b8b805c
Ganeshgit123 <liai.ganeshkumar@gmail.com>
2021-07-07 07:34:07 +0530

    jul 7  morning commit

--- src/app/about-page/about-page.component.ts
+++ src/app/about-page/about-page.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder,Validators} from '@angular/forms';
+import { ApiCallService } from '../services/api-call.service';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import * as DecoupledEditor from '@ckeditor/ckeditor5-build-decoupled-document';
 
 @Component({
   selector: 'app-about-page',
@@ -6,10 +10,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about-page.component.scss']
 })
 export class AboutPageComponent implements OnInit {
+  breadCrumbItems: Array<{}>;
 
-  constructor() { }
+  imagePreview = null;
+  fileUpload: any;
+
+  public Editor = DecoupledEditor;
+  public onReady( editor ) {
+     editor.ui.getEditableElement().parentElement.insertBefore(
+         editor.ui.view.toolbar.element,
+         editor.ui.getEditableElement()
+     );
+ }
+
+  constructor(private formBuilder: FormBuilder,
+    private apiCall: ApiCallService,
+    private modalService: NgbModal
+    ) { }
 
   ngOnInit(): void {
+    this.breadCrumbItems = [{ label: 'CMS' }, { label: 'About Page', active: true }];
+
   }
+
+  uploadImageFile(event){
+    var reader = new FileReader();
+      reader.onload = (event: any) => {
+        this.imagePreview = event.target.result;
+      }
+      reader.readAsDataURL(event.target.files[0]); 
+      this.fileUpload = event.target.files[0]
+  }
+
 
 }
