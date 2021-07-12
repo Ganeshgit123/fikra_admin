a63c0ca22491268c986ad32c40bd5b95ab1c9edb
Ganeshgit123 <liai.ganeshkumar@gmail.com>
2021-07-12 07:19:00 +0530

    jun12 morning commit

--- src/app/investors/investors.component.ts
+++ src/app/investors/investors.component.ts
@@ -8,70 +8,6 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 import { AdvancedSortableDirective, SortEvent,SortDirection } from '../services/advanced-sortable.directive';
 
-interface Table {
-  fullName: any;
-  userName: any;
-  country: any;
-  city: any;
-  street: any;
-  mobileNumber: any;
-  _send_me_newsletter_: any;
-  _userRole_: any;
-  _is_mobile_verified_: any;
-  _is_admin_arroved_: any;
-  _id:any;
-}
-interface SearchResult {
-  tables: Table[];
-  total: number;
-}
-
-interface State {
-  page: number;
-  pageSize: number;
-  searchTerm: string;
-  sortColumn: string;
-  sortDirection: SortDirection;
-  startIndex: number;
-  endIndex: number;
-  totalRecords: number;
-}
-
-function compare(v1, v2) {
-  return v1 < v2 ? -1 : v1 > v2 ? 1 : 0;
-}
-
-/**
-* Sort the table data
-* @param tabless Table field value
-* @param column Fetch the column
-* @param direction Sort direction Ascending or Descending
-*/
-function sort(tables: Table[], column: string, direction: string): Table[] {
-  if (direction === '') {
-      return tables;
-  } else {
-      return [...tables].sort((a, b) => {
-          const res = compare(a[column], b[column]);
-          return direction === 'asc' ? res : -res;
-      });
-  }
-}
-
-/**
-* Table Data Match with Search input
-* @param tables Table field value fetch
-* @param term Search the value
-*/
-function matches(tables: Table, term: string, pipe: PipeTransform) {
-  return tables.fullName.toLowerCase().includes(term)
-      || tables.userName.toLowerCase().includes(term)
-      || tables.country.toLowerCase().includes(term)
-      || pipe.transform(tables.city).includes(term)
-      || tables.street.toLowerCase().includes(term)
-      || tables.mobileNumber.toLowerCase().includes(term);
-}
-
 @Component({
   selector: 'app-investors',
   templateUrl: './investors.component.html',
@@ -86,31 +22,24 @@ export class InvestorsComponent implements OnInit {
  _user_ID_ : any;
 
  getuserList: any=[];
+ getfieldList: any=[];
+ fieldata:any;
+ fieldId:any;
+ userFiedld :any;
+ userFeed: any;
+ filedid:any;
+ datalist:any;
+ new_arr: any=[];
+ values:any;
+ sorting: any=[];
+ abc: any=[];
+ searchTerm;
 
  accToken = sessionStorage.getItem('access_token');
 
   updatedby = sessionStorage.getItem('adminId');
   role = sessionStorage.getItem('adminRole');
 
- private _loading$ = new BehaviorSubject<boolean>(true);
-    // tslint:disable-next-line: variable-name
-    private _search$ = new Subject<void>();
-    // tslint:disable-next-line: variable-name
-    private _tables$ = new BehaviorSubject<Table[]>([]);
-    // tslint:disable-next-line: variable-name
-    private _total$ = new BehaviorSubject<number>(0);
-
-        // tslint:disable-next-line: variable-name
-        private _state: State = {
-          page: 1,
-          pageSize: 10,
-          searchTerm: '',
-          sortColumn: '',
-          sortDirection: '',
-          startIndex: 1,
-          endIndex: 10,
-          totalRecords: 0
-      };
 
       
  @ViewChildren(AdvancedSortableDirective) headers: QueryList<AdvancedSortableDirective>;
@@ -118,54 +47,8 @@ export class InvestorsComponent implements OnInit {
  constructor(private pipe: DecimalPipe,
   private apiCall: ApiCallService,
   private formBuilder: FormBuilder,) {
-  //  this.tables$ = service.tables$;
-  //  this.total$ = service.total$;
-   this._search$.pipe(
-    tap(() => this._loading$.next(true)),
-    debounceTime(200),
-    switchMap(() => this._search()),
-    delay(200),
-    tap(() => this._loading$.next(false))
-).subscribe(result => {
-    this._tables$.next(result.tables);
-    this._total$.next(result.total);
-});
 
-this._search$.next();
  }
-
-     /**
-     * Returns the value
-     */
-      get tables$() { return this._tables$.asObservable(); }
-      get total$() { return this._total$.asObservable(); }
-      get loading$() { return this._loading$.asObservable(); }
-      get page() { return this._state.page; }
-      get pageSize() { return this._state.pageSize; }
-      get searchTerm() { return this._state.searchTerm; }
-      get startIndex() { return this._state.startIndex; }
-      get endIndex() { return this._state.endIndex; }
-      get totalRecords() { return this._state.totalRecords; }
-  
-      /**
-       * set the value
-       */
-      // tslint:disable-next-line: adjacent-overload-signatures
-      set page(page: number) { this._set({ page }); }
-      // tslint:disable-next-line: adjacent-overload-signatures
-      set pageSize(pageSize: number) { this._set({ pageSize }); }
-      // tslint:disable-next-line: adjacent-overload-signatures
-      // tslint:disable-next-line: adjacent-overload-signatures
-      set startIndex(startIndex: number) { this._set({ startIndex }); }
-      // tslint:disable-next-line: adjacent-overload-signatures
-      set endIndex(endIndex: number) { this._set({ endIndex }); }
-      // tslint:disable-next-line: adjacent-overload-signatures
-      set totalRecords(totalRecords: number) { this._set({ totalRecords }); }
-      // tslint:disable-next-line: adjacent-overload-signatures
-      set searchTerm(searchTerm: string) { this._set({ searchTerm }); }
-      set sortColumn(sortColumn: string) { this._set({ sortColumn }); }
-      set sortDirection(sortDirection: SortDirection) { this._set({ sortDirection }); }
-  
 
 
  ngOnInit() {
@@ -188,6 +71,7 @@ this._search$.next();
   });
  }
 
+ 
 
  _fetchData() {
 
@@ -198,11 +82,60 @@ this._search$.next();
     let resu = result.body;
     if(resu.error == false)
     {
-         this.getuserList = resu.data;
-   this.getuserList.forEach(element => {
-    element['isEdit'] = false;
-  });
-        //  console.log("list",this.getuserList)
+      this.getfieldList = resu.fields;
+      this.getuserList = resu.data;
+
+      function arr_diff (a1, a2) {
+
+        var a = [], diff = [];
+      
+        for (var i = 0; i < a1.length; i++) {
+            a[a1[i]] = true;
+        }
+      
+        for (var i = 0; i < a2.length; i++) {
+            if (a[a2[i]]) {
+                delete a[a2[i]];
+            } else {
+                a[a2[i]] = true;
+            }
+        }
+      
+        for (var k in a) {
+            diff.push(k);
+        }
+      
+        return diff;
+      }
+      
+      const field_name = this.getfieldList.map((it)=>{
+        return it.fieldId
+      })
+      
+      const det = this.getuserList.filter((dt,idx)=>{
+        return idx === 0
+      })
+      
+       this.values = Object.keys(det[0]);
+      
+      let diff_ele = arr_diff(this.values, field_name)
+      
+      this.new_arr = this.getuserList.filter((item, idx)=>{
+        return diff_ele.map((e)=>{
+          delete item[e]
+        })
+        
+      })
+
+      this.abc = this.new_arr.map((item)=>{
+        const a = {}
+        field_name.forEach((f)=>{
+          a[f] = item[f]
+
+        })
+        return item = a
+      })
+
     }else{
       this.apiCall.showToast(resu.message, 'Error', 'errorToastr')
     }
@@ -211,88 +144,10 @@ this._search$.next();
      
   });
 
-
  }
 
- getTableData(data){
-   console.log("fef",data)
-   data.isEdit = true;
-
-  //  var dob = new Date(data['dob']);
-
-    this._user_ID_ = data['_id'] 
-
-   this.updateInvestor   = this.formBuilder.group({
-    fullName: [data['fullName'],  [ Validators.required, Validators.pattern(/^(?!\s*$).+/)]],
-    userName: [data['userName'],  [ Validators.required, Validators.pattern(/^(?!\s*$).+/)]],
-    country: [data['country'],  [ Validators.required, Validators.pattern(/^(?!\s*$).+/)]],
-    city: [data['city'],  [ Validators.required, Validators.pattern(/^(?!\s*$).+/)]],
-    street: [data['street'],  [ Validators.required, Validators.pattern(/^(?!\s*$).+/)]],
-    mobileNumber: [data['mobileNumber'],  [ Validators.required, Validators.pattern(/^(?!\s*$).+/)]],
-    // dob: [dob, [ Validators.required, Validators.pattern(/^(?!\s*$).+/)]],
-    _send_me_newsletter_: [data['_send_me_newsletter_'],  [ Validators.required, Validators.pattern(/^(?!\s*$).+/)]],
-    _is_admin_arroved_: [data['_is_admin_arroved_'],  [ Validators.required, Validators.pattern(/^(?!\s*$).+/)]],
-    _userRole_: [data['_userRole_'],  [ Validators.required, Validators.pattern(/^(?!\s*$).+/)]],
-  })
- }
-
- close(data){
-  data.isEdit = false;
- }
-
- onSubmit(){
-
- }
-
-
- /**
-  * Sort table data
-  * @param param0 sort the column
-  *
-  */
- onSort({ column, direction }: SortEvent) {
-   // resetting other headers
-   this.headers.forEach(header => {
-     if (header.sortable !== column) {
-       header.direction = '';
-     }
-   });
-   this.sortColumn = column;
-   this.sortDirection = direction;
- }
- private _set(patch: Partial<State>) {
-  Object.assign(this._state, patch);
-  this._search$.next();
-}
-
-/**
-* Search Method
-*/
-
- private _search(): Observable<SearchResult> {
-  const { sortColumn, sortDirection, pageSize, page, searchTerm } = this._state;
-
-  // 1. sort
-  let tables = sort(this.getuserList, sortColumn, sortDirection);
-  // 2. filter
-  tables = tables.filter(table => matches(table, searchTerm, this.pipe));
-  const total = tables.length;
-
-  // 3. paginate
-  this.totalRecords = tables.length;
-  
-  this._state.startIndex = (page - 1) * this.pageSize + 1;
-
-  this._state.endIndex = (page - 1) * this.pageSize + this.pageSize;
-
-  if (this.endIndex > this.totalRecords) {
-      this.endIndex = this.totalRecords;
-  }
-  // tables = tables.slice(this._state.startIndex - 1, this._state.endIndex - 1);
-
-  return of(
-      { tables, total }
-  );
+ getData(item) {
+  return Object.keys(item);
 }
 
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   //  baseUrl: 'http://127.0.0.1:3002/'
-  baseUrl: 'http://15.184.187.254:8080/'
+  // baseUrl: 'http://15.184.187.254:8080/'
+  baseUrl: 'https://fikra.app/api/'
 };
